fdf91f0bd7b883b04c51acdf8a88ba73f8d0572a
drout0 <lawrencebr@hotmail.com>
2020-03-13 00:15:04 -0300

    Update ps-rear-view-camera.js

--- ps-rear-view-camera.js
+++ ps-rear-view-camera.js
@@ -298,9 +298,6 @@ function render() {
 
 	// Student: set rearCam so it's pointing in the opposite direction than the camera
 
-	//rearCam.position = camera.position;
-	//rearCam.position.x += 50;
-
 
 	// rearview render
 	renderer.enableScissorTest( true );
