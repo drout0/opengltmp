0c9e9239dd322152ee4c239a65b2aaf8c99d1745
drout0 <lawrencebr@hotmail.com>
2020-05-16 20:37:20 -0300

    Update ps-rear-view-camera.js

--- ps-rear-view-camera.js
+++ ps-rear-view-camera.js
@@ -19,7 +19,7 @@ function fillScene() {
 	
 // ArrowHelper	
 var sourcePos = new THREE.Vector3(0, 0, 0);
-var targetPos = new THREE.Vector3(0, 50, 0);
+var targetPos = new THREE.Vector3(0, 0, 50);
 var direction = new THREE.Vector3().sub(targetPos, sourcePos);
 var arrow = new THREE.ArrowHelper(direction.clone().normalize(), sourcePos, direction.length(), 0x00ff00);
 scene.add(arrow);
